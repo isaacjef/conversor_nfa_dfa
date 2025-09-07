1c9a57a33ae3332d1c2e028fbd488d45c5414b54
isaacjef <isaac.conceicao@estudante.ifgoiano.edu.br>
2025-09-06 21:23:09 -0300

    ttestes 06/09

--- src/main/java/src/DFA.java
+++ src/main/java/src/DFA.java
@@ -42,6 +42,7 @@ public class DFA implements AutomatoFinito {
      * @param NFA que deseja converter em um DFA
      */
     public void DFAfromNFA(NFA nfa1){
+    //public Map<Object, Map<String, List<String>>> DFAfromNFA(NFA nfa1){
         /*
          * Desenvolver a linha de código responsavel pela converção
          * do NFA em um DFA. Seguindo os passos da sala de aula.
@@ -56,47 +57,52 @@ public class DFA implements AutomatoFinito {
         
         // Passo 2:  Criar todas as combinações possíveis entre os estados
         ArrayList<Object> teste = gerarConjunto(nfa1.getStates());
-
+    
         // Passo 1: Definir a quantidade de estados do DFA
         teste.size();
 
         // Passo 3: Definir a transição de cada estado
         Map<Object, Map<String, List<String>>> matriz = new HashMap<>();
-        ArrayList<String> estadosInitalEnd = new ArrayList<>();
+        ArrayList<String> estadosInitialEnd = new ArrayList<>();
 
         for (Object chavePrincipal : teste) {
 
             Map<String, List<String>> matrizAux = new HashMap<>();
             Set<String> endList = new HashSet<>();
 
-            for (String alphabet : this.alphabet) {
+            for (String alpha : this.alphabet) {
                 
                 // Verifica se chaveprincipal de busca é do tipo List, para tratar quando "null"
                 if (chavePrincipal instanceof List) {
                     List<String> chavePrincList = (List<String>) chavePrincipal;
+                    List<String> end = new ArrayList<>();
 
-                    // Percorre cada valor contido na
+                    //System.out.println(chavePrincList);
+                    // Percorre cada valor contido na matriz/tabela de estados possiveis
                     for (String estadoNFA : chavePrincList) {
-                        
-                        List<String> end = 
-                        nfa1.getTransiction().getOrDefault(estadoNFA, Collections.emptyMap()).get(alphabet);
-                        
+                        //System.out.println("FORA: " + chavePrincList + " | " + estadoNFA);
+                        System.out.println("Estado NFA: " + chavePrincList + " | Alfabeto " + alpha);
+                        end = 
+                        nfa1.getTransiction().getOrDefault(estadoNFA, Collections.emptyMap()).get(alpha);
+                        System.out.println("TRANSICAO: " + nfa1.getTransiction().getOrDefault(estadoNFA, Collections.emptyMap()).get(alpha));
+                        System.out.println("TRANSICAO end: " + end);
                         if (end != null) {
                             endList.addAll(end);
-                        }
+                            //System.out.println("TRANSICAO endList: " + endList);
+                        }  
                     }
                 }
 
                 List<String> destinoFinal = new ArrayList<>(endList);
                 Collections.sort(destinoFinal);
 
-                matrizAux.put(alphabet, destinoFinal);
+                matrizAux.put(alpha, destinoFinal);
 
             }
 
             matriz.put(chavePrincipal, matrizAux);
-
-            // Passo 4: Definir o estado inicial e os estados finais
+            System.out.println(matriz);
+            // Passo 4: Definir o estado inicial e os estados finais -----------------------------
             if (chavePrincipal instanceof List) {
                 boolean isInitial = false;
                 boolean isFinal = false;
@@ -108,23 +114,28 @@ public class DFA implements AutomatoFinito {
                     isFinal = true;
 
                 if (isInitial && isFinal) 
-                    estadosInitalEnd.add("->*");
+                    estadosInitialEnd.add("->*");
                 
                 else if (isInitial)
-                    estadosInitalEnd.add("->");
+                    estadosInitialEnd.add("->");
 
                 else if (isFinal)
-                    estadosInitalEnd.add("*");
+                    estadosInitialEnd.add("*");
 
                 else
-                    estadosInitalEnd.add("");
-                
+                    estadosInitialEnd.add("");
             } 
-            
         }
 
         // Passo 5: Renomear o conjunto de estados
-        Map<String, ArrayList<Object>> tabelaRenomeda = new HashMap<>();
+        Map<Object, String> tabelaRenomeada = new HashMap<>();
+        int i = 0;
+
+        for (Object chavePrincipal : teste) {
+            String name = "A" + i;
+            tabelaRenomeada.put(chavePrincipal, name);
+            i++;
+        }
 
         // Passo 6: Descartar os estados inacessíveis
         /*
@@ -149,10 +160,53 @@ public class DFA implements AutomatoFinito {
           * [q1, q2]     | null      | [q2]      |
           * [q0, q1, q2] | [q0, q1]  | [q0, q2]  |
          *
+         * 
+         * f(list_rename.isEmpty()) {
+                    tabelaRenomeda.put(alphabet, teste)
+                }/
+         * 
          */
+        /*Map<String, Map<String, String>> passo6 = new HashMap<>();
+        String renomear = "";
+        String aba="";
+        //{ Código Teste Isaac
+        //Análogo ao FOR de cima
+        for (Object chavePrincipal : teste) {
 
+            Map<String, String> matrizAux2 = new HashMap<>();
+
+            for (String alpha : this.alphabet) {
+
+                //System.out.println("aba: " + aba);
+                List<String> list_aux = matriz.get(chavePrincipal).get(alpha);
+                System.out.println("aba: " + matriz.get(chavePrincipal));
+                //System.out.println("List aux: " + list_aux);
+                if (chavePrincipal == null || list_aux.contains("")) {
+                    renomear = "A0";
+                } else {
+                    renomear = (tabelaRenomeada.get(list_aux));
+                }
+                //System.out.println("List aux: " + list_aux);
+                //Map<String, List<String>> matrizAux2 = new HashMap<>();
+                // if (tabelaRenomeada.containsKey(list_aux)) {
+                //     renomear = (tabelaRenomeada.get(list_aux));
+                //     //System.out.println("List aux: " + renomear);
+                // }
+                matrizAux2.put(alpha, renomear);
+                //System.out.println("matriz aux: " + matrizAux2);
+                //passo6.put(aba, matrizAux2);
+            }
+            aba = tabelaRenomeada.get(chavePrincipal);
+            passo6.put(aba, matrizAux2);
+        }
+        
         //Passo 7: Atribuir resultado ao atributo this.transiction
 
+        System.out.println("Todas as combinações: " + teste);
+        System.out.println("Estados possiveis, entradas e saídas: " + matriz);
+        //System.out.println("Estados inicial final: " + estadosInitalEnd);
+        System.out.println("\nTabela renomeada: " + tabelaRenomeada);
+        System.out.println("\nResultado final: " + passo6);*/
     }
 
     // Conversor de DFa em Json, ao final deve gerar o arquivo json
@@ -195,6 +249,7 @@ public class DFA implements AutomatoFinito {
      * @param Estados do NFA a ser convertido
      */
     public ArrayList<Object> gerarConjunto(ArrayList<String> states) {
+    //public ArrayList<String> gerarConjunto(ArrayList<String> states) {
 
         ArrayList<List<String>> conjuntoDasPartes = new ArrayList<>();
         conjuntoDasPartes.add(new ArrayList<>()); //Inicializa como conjunto vazio
@@ -220,14 +275,12 @@ public class DFA implements AutomatoFinito {
         // Organiza cada elemento com relação ao tamanha do array que o compõem.
         conjuntoDasPartes.sort(Comparator.comparingInt(List::size));
 
-        ArrayList<Object> conjuntoOrganizado = new ArrayList<>();
+        ArrayList<Object> conjuntoOrganizado = new ArrayList<Object>();
         for (List<String> aux : conjuntoDasPartes) {
-
             if (aux.isEmpty())
-                conjuntoOrganizado.add("null");
+                conjuntoOrganizado.add(null);
             else
                 conjuntoOrganizado.add(aux);
-
         }
         //conjuntoDasPartes.forEach(aux -> aux.isEmpty() ? resultadoFormatado.add("null"): resultadoFormatado.add(aux));
 
@@ -237,43 +290,59 @@ public class DFA implements AutomatoFinito {
     /*
      * Divisão do código para gets e seters
      */
+    @Override
     public ArrayList<String> getAlphabet(){
         return this.alphabet;
     }
 
+    @Override
     public void setAlphabet(ArrayList<String> alphabet){
         this.alphabet = alphabet;
     }
 
+    @Override
     public ArrayList<String> getEnd_state() {
         return this.end_state;
     }
 
+    @Override
     public void setEnd_state(ArrayList<String> end_state) {
         this.end_state = end_state;
     }
 
+    @Override
     public ArrayList<String> getStates() {
         return this.states;
     }
 
+    @Override
     public void setStates(ArrayList<String> states) {
         this.states = states;
     }
 
+    @Override
     public String getInitial_state() {
         return this.initial_state;
     }
 
+    @Override
     public void setInitial_state(String initial_state) {
         this.initial_state = initial_state;
     }
 
+    @Override
     public Map<String, Map<String, List<String>>> getTransiction() {
         return this.transiction;
     }
 
+    @Override
     public void setTransiction(Map<String, Map<String, List<String>>> transiction) {
         this.transiction = transiction;
     }
 }
+
+/**
+ * Classe NFA concluída
+ * @author Gabriel Alexandre
+ * @author github.com/isaacjef/
+ */
