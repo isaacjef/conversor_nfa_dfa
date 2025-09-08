2ddbb0fe7640d3cd339e05cc5b3401da4f81e043
isaacjef <isaacjeferson143@gmail.com>
2025-09-08 18:29:56 -0300

    Update Main.java

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -16,11 +16,11 @@ public class Main {
      *  Função principal de execução
      *
      */
-    //@SuppressWarnings("unchecked")
+
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String diretorioString="C:\\Users\\Pichau\\Desktop\\conversor_nfa_dfa\\exemplo.json";
+        String diretorioString="";
         JSONObject jsonObject;
         JSONParser parser = new JSONParser();
         ArrayList<NFA> listaNFA = new ArrayList<>();
@@ -34,8 +34,8 @@ public class Main {
          * 
          * Fica a critério qual usaremos.
          */
-        //System.out.print("\nInforme o diretório do JSON: ");
-        //diretorioString = sc.nextLine();
+        System.out.print("\nInforme o diretório do JSON: ");
+        diretorioString = sc.nextLine();
 
         try {
             Object objetoJSON = parser.parse(new FileReader(diretorioString));
